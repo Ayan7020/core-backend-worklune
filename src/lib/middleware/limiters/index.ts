a887be1f649a65c7c3026bb94197c7f72a22f495
alqasim-hajj <alqasim586@gmail.com>
2026-01-05 14:27:06 +0530

    feat: refactor rate limiting logic to use access token for user identification

--- src/lib/middleware/limiters/index.ts
+++ src/lib/middleware/limiters/index.ts
@@ -5,19 +5,28 @@ import { anomLimiter } from "./anomLimiter";
 import { asyncHandler } from "@/utils/asyncHandler";
 import { TooManyRequestError } from "@/utils/errors/HttpErrors";
 import { AppError } from "@/utils/errors/AppError";
+import { verifyAccessToken } from "@/utils/jwtHelper";
+import { JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
 
 export const RateLimit = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  const ip = req.ip;
+  if (!ip) {
+    throw new Error("Invalid Request!");
+  }
   try {
-    const userId = req.user?.id;
-    const ip = req.ip;
-    if (userId) {
-      await userLimiter.consume(userId, 1);
-    } else {
-      if (!ip) {
-        throw new Error("Invalid Request!");
+    const access_token = req.cookies?.access_token;
+    if (access_token) {
+      try {
+        const payload = verifyAccessToken(access_token);
+        const userId = String(payload.sub);
+        await userLimiter.consume(userId, 1);
+      } catch (err) { 
+        await anomLimiter.consume(ip, 1);
       }
+    } else {
       await anomLimiter.consume(ip, 1);
-    }
+    } 
+    
     await globalLimiter.removeTokens(1);
 
     next();
